de9f8b8c90c52cc8afa08573eb712d1a02acb936
Alexandra <thedatacurious@gmail.com>
2021-07-16 13:48:21 +0800

    Updated public directory path

--- gh-pages.js
+++ gh-pages.js
@@ -1,10 +1,10 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(
-    'public', // path to public directory
+    './public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/thedatacurious/2020-census-gender-roles.git', // Update to point to your repository  
+        repo: 'https://github.com/thedatacurious/2020-census-gender-roles.git', // Update to point to your repository
         user: {
             name: 'Alexandra Khoo', // update to use your name
             email: 'thedatacurious@gmail.com' // Update to use your email
